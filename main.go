fa7c6aaf12ac0524c26a0997e8cc63ff74920371
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-11-01 18:22:24 -0300

    features(create func AddTask, ListTask, SaveTask)

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 
 // OS proporciona funciones y métodos para interactuar con el sistema operativo.
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
+	"strings"
 
-	"github.com/LuisZentenxx/go-cli-crud/tasks"
+	task "github.com/LuisZentenxx/go-cli-crud/tasks"
 )
 
 // Es el punto de entrada de cualquier programa Go.
 func main() {
 
-	file, err := os.OpenFile("tasks.json", os.O_RDWR, 0666)
+	file, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0666)
 	// Abre o crea, si no existe, un archivo llamado "tasks.json" en modo lectura y escritura.
 	// `os.O_RDWR` indica que se abrirá en modo lectura y escritura.
 	// `0666` representa los permisos dados al archivo una vez abierto.
@@ -27,25 +30,47 @@ func main() {
 	defer file.Close()
 	// Utiliza `defer` para asegurarse de que el archivo se cierre después de que la función `main()` termine o si se produce un error.
 	// `defer` pospone la ejecución de `file.Close()` hasta que `main()` termine.
-	
-	var tasks []tasks.Task 
-	info, err := file.Stat()
 
+	var tasks []task.Task
+
+	info, err := file.Stat()
 	if err != nil {
 		panic(err)
 	}
-	
+
 	if info.Size() != 0 {
 		bytes, err := io.ReadAll(file)
-		
 		if err != nil {
 			panic(err)
 		}
 
-		json.Unmarshal(byte)
+		//
+		err = json.Unmarshal(bytes, &tasks)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		tasks = []task.Task{}
 	}
 
-	else {
-		
+	if len(os.Args) < 2 {
+		printUsage()
 	}
+
+	switch os.Args[1] {
+	case "list":
+		task.ListTasks(tasks)
+	case "add":
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Which is yout task")
+		name, _ := reader.ReadString('\n')
+		name = strings.TrimSpace(name)
+
+		tasks = task.AddTask(tasks, name)
+		fmt.Println(tasks)
+	}
+}
+
+func printUsage() {
+	fmt.Println("Uso : go-cli-crud [list|add|complete|delete]")
 }
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,7 +1,58 @@
-package tasks
+package task
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type Task struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Complete bool   `json:"complete"`
+}
+
+func ListTasks(tasks []Task) {
+	if len(tasks) == 0 {
+		fmt.Println("No tasks to list")
+		return
+	}
+
+	for _, task := range tasks {
+
+		status := "✓"
+		if task.Complete {
+			status = ""
+		}
+
+		fmt.Printf("[%s] %d %s\n", status, task.ID, task.Name)
+	}
+
+}
+
+func AddTask(tasks []Task, name string) []Task {
+	newTask := Task{
+		ID:       10,
+		Name:     name,
+		Complete: false,
+	}
+	return append(tasks, newTask)
+}
+
+func SaveTask(file *os.File, tasks []Task) {
+	bytes, err := json.Marshal(tasks)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err := file.Seek(0, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	err = file.Truncate(0)
+	if err != nil {
+		panic(err)
+	}
+
 }
